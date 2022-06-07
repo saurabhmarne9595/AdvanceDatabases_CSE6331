462d12ca1c3d532fe6a733e464c063da3bd6a58d
Saurabh Marne <40160235+saurabhmarne9595@users.noreply.github.com>
2022-06-06 21:39:02 -0500

    assignment 2

--- app.py
+++ app.py
@@ -4,121 +4,112 @@ from flask import Flask, render_template, request
 import pyodbc
 from azure.storage.blob import BlobServiceClient
 ############### 	Database connection #############
-#Name	State	Salary	Grade	Room	Telnum	Picture	Keywords
 
 server ='tcp:adbassignmentserver1.database.windows.net' 
 database ='adbassignment1' 
 username ='saurabh'
-password ='password'
+password ='my_password'
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = conn.cursor()
 
-# Blob object for connection.
-conn_blob="DefaultEndpointsProtocol=https;AccountName=storageadb01;AccountKey=ony7qoLaKWLAH7VVsndj19UxwYx93P0VKLP5Kd9YCji1hXDQsrc5rv7bFZnW7I/acOTar0O+5iRB9UK4MiFoWQ==;EndpointSuffix=core.windows.net"
-container_name = "containeradb01" # container name in which images will be store in the storage account
-blob_service_client = BlobServiceClient.from_connection_string(conn_str=conn_blob)
-try:
-    container_client = blob_service_client.get_container_client(container=container_name)
-    container_client.get_container_properties()
-except Exception as e:
-    container_client = blob_service_client.create_container(container_name)
-
 app = Flask(__name__)
-
 @app.route('/',methods=['GET','POST'])
 def hello_world(): 	
-	return render_template("home.html")
+    query="SELECT TOP (5) * FROM [dbo].[ass2_all_month]"
+    cursor.execute(query) 
+    row = cursor.fetchall()
+    return render_template('home.html', variable=row)
 
-@app.route('/all')
-def all_doc():
-	cursor.execute("SELECT * from dbo.people;") 
-	row = cursor.fetchall()
-	return str(row)
-
-@app.route("/searchname", methods=['GET','POST'])
-def searchName():
-    findname = str(request.args.get('searchname'))
-    print(findname)
-    cursor.execute("select * from dbo.people where Name = '"+ findname+ "';")
-    getdata = cursor.fetchall()
-    print(getdata)
-    return render_template("names.html", variable=getdata[0][6] )
-
-@app.route("/getsalary", methods=['GET','POST'])
-def checkSalary():
-    salary= str(request.args.get('salary'))
-    cursor.execute("select Name, Salary, Picture  from dbo.people where Salary < '"+ salary +"';")
+@app.route('/largest',methods=['GET','POST'])
+def largest(): 	
+    findN=str(request.args.get('n'))
+    '''     select * from dbo.ass2_all_month where ( mag IN (
+                SELECT top 5 mag FROM dbo.ass2_all_month as table1 group by mag order by mag desc )    );
+    '''
+    query="select * from dbo.ass2_all_month where ( mag IN ( SELECT top "+ findN +" mag FROM dbo.ass2_all_month as table1 group by mag order by mag desc));"
+    cursor.execute(query) 
     rows = cursor.fetchall()
-    newrows=[]
-    for i in rows:
-        if i[0] != None:
-            newrows.append((i[0], i[1], i[2])   )
-    return render_template("salary.html", setsalnames = rows, salary=salary)
+    return render_template('largest.html', rows=rows)
 
-
-@app.route("/viewPhoto")
-def view_photos():
-    return render_template("checkphoto.html")
-
-@app.route("/picture", methods=["POST"])
-def upload_photos():
-    sname=(request.form["searchname"])
-    filenames = ""
-    for file in request.files.getlist("photos"):
-        try:
-            container_client.upload_blob(file.filename, file)
-            filenames = file.filename
-            cursor.execute("UPDATE dbo.people SET Picture = '" + filenames + "' WHERE Name='" + sname + "';")
-            cursor.commit()
-        except Exception as e:
-            # print(e)
-            print("Ignoring duplicate filenames")  # ignore duplicate filenames
-    return render_template("picture.html", finalpicturename =filenames, setname = sname)
-
-@app.route("/edit")
-def changesalary():
-    findname = str(request.args.get('name'))
-    cursor.execute("select * from dbo.people where Name = '"+findname+"';")
+@app.route('/nearArlington',methods=['GET','POST'])
+def nearArlington(): 	
+    # findN=str(request.args.get('n'))
+    '''     select * from dbo.ass2_all_month where ( mag IN (
+                SELECT top 5 mag FROM dbo.ass2_all_month as table1 group by mag order by mag desc )    );
+        32.7357° N, 97.1081° W
+        32.729641, -97.110566
+        select * from dbo.ass2_all_month where longitude BETWEEN  44.729641 and 20.229641 ;
+    '''
+    query= "SELECT longitude, latitude, place, [time] FROM dbo.ass2_all_month WHERE longitude BETWEEN '-110' AND '-89' AND latitude BETWEEN  '23' and '49';"
+    cursor.execute(query) 
     rows = cursor.fetchall()
-    print(rows)
-    counters=0
-    if(len(rows)!=0):
-        getstate = str(request.args.get('state'))
-        if(getstate==''):
-                getstate=str(rows[0][1])
-        getsalary = str(request.args.get('salary'))
-        if(getsalary==''):
-            getsalary=str(rows[0][2])
-        getgrade = str(request.args.get('grade'))
-        if(getgrade==''):
-            getgrade=str(rows[0][3])
-        getroom = str(request.args.get('room'))
-        if(getroom==''):
-            getroom=str(rows[0][4])
-        gettelnum= str(request.args.get('telnum'))
-        if(gettelnum==''):
-            gettelnum=str(rows[0][5])
-        getkeywords= str(request.args.get('keywords'))
-        if(getkeywords==''):
-            getkeywords=str(rows[0][7])
-        cursor.execute("UPDATE dbo.people SET State = '"+getstate+"', Salary = "+getsalary+",Grade ="+getgrade+",Room ="+getroom+",Telnum = "+gettelnum+", Keywords = '"+getkeywords+"' WHERE Name='"+findname+"';")
-        cursor.commit()
-        counters=1
-    print(findname)
-    return render_template("edit.html", counter=counters )
+    return render_template('nearArlington.html', rows=rows)
 
-
-@app.route("/deletedata")
-def deletedata():
-    findname = str(request.args.get('searchname'))
-    cursor.execute("select * from dbo.people where Name = '"+findname+"';")
+@app.route('/recentRange',methods=['GET','POST'])
+def recentRange(): 	
+    getmindate = str(request.args.get('firstdate'))
+    getmaxdate = str(request.args.get('lastdate'))
+    print(getmaxdate)
+    print(getmindate)
+    query = " select top 20 t.blah as [score range], count(*) as [number of occurences] from ( select case when mag between  1 and  2 then ' 1-2 ' when mag between  2 and  3 then ' 2-3 ' when mag between  3 and  4 then ' 3-4 ' when mag between  3 and  4 then ' 4-5 ' when mag between  5 and  6 then ' 5-6 '  when mag between  6 and  7 then ' 6-7 '  else '8-above' end as blah from ass2_all_month where time>='"+getmindate+' "AND time<= ' + getmaxdate + "') t group by t.blah ;"
+    cursor.execute(query) 
     rows = cursor.fetchall()
-    counters=0
-    if(len(rows)!=0):
-        cursor.execute("DELETE FROM dbo.people WHERE Name='"+findname+"';")
-        cursor.commit()
-        counters=1
-    return render_template("deletedata.html", counter=counters)
+    return render_template('recentRange.html', rows=rows)
+
+# count of the quaakes betwwen given range: SELECT COUNT(id) from ass2_all_month where mag BETWEEN 1 and 2; 
+# SELECT * FROM [dbo].[all_months] WHERE TIME BETWEEN '2022-01-20 00:00:00.000' AND '2022-01-26 23:59:59.000' AND mag > 3
+#SELECT longitude, latitude, place, [time] FROM dbo.ass2_all_month WHERE longitude BETWEEN '-99' AND '-93' AND latitude BETWEEN  '32' and '39';
+
+@app.route('/anchorage',methods=['GET','POST'])
+def anchorage(): 	
+    getlongitude = str(request.form.get('longi'))
+    getlatitude = str(request.form.get('lati'))
+    getsurrounding = str(request.form.get('surrounding'))
+    leftlatitude=float(getlatitude) - float(getsurrounding)/111
+    rightlatitude=float(getlatitude) + float(getsurrounding)/111
+    bottomlongitude=float(getlongitude) - float(getsurrounding)/111
+    toplongitude=float(getlongitude) + float(getsurrounding)/111
+    cursor.execute("select top 1 mag, place, time, latitude, longitude from dbo.all_month where (latitude>= "+str(leftlatitude)+" AND latitude<= "+str(rightlatitude)+" ) AND ( longitude>= "+str(bottomlongitude)+" AND longitude<= "+str(toplongitude)+") order by mag desc ;")
+    rows = cursor.fetchall()
+    return render_template('anchorage.html', rows=rows)
+    
+
+#################################### quiz 2
+@app.route('/q10',methods=['GET','POST'])
+def q10(): 	
+    getlat = str(request.args.get('latitude'))
+    getdeg = str(request.args.get('degrees'))
+    lat2 = str(int(getlat)+ int(getdeg))
+    lat1 = str(int(getlat)- int(getdeg))
+    query = " SELECT time, latitude, longitude, id,  place from eq where latitude between "+  lat1+" and "+ lat2+";"
+    cursor.execute(query) 
+    rows = cursor.fetchall()
+    return render_template('q10.html', rows=rows)
+
+@app.route('/q12',methods=['GET','POST'])
+def q12(): 	
+    getmaxmag = str(request.args.get('maxmag'))
+    getminmag = str(request.args.get('minmag'))
+    getplace = str(request.args.get('place'))
+    query = "SELECT time, latitude, longitude, mag, place from eq where  place LIKE '%"+getplace+"%' and mag between "+ getminmag +" and "+getmaxmag+   ";"
+    cursor.execute(query) 
+    rows = cursor.fetchall()
+    return render_template('q12.html', rows=rows)
+
+@app.route('/q11',methods=['GET','POST'])
+def q11(): 	
+    getn = str(request.args.get('n'))
+    getnet = str(request.args.get('net'))
+    getsamll = str(request.args.get('smalln'))
+    getlarge = str(request.args.get('largen'))
+    query = "UPDATE eq SET gap = "+ getn + " WHERE net = '"+ getnet+"';"
+    cursor.execute(query) 
+    #        select count(id) from eq where gap BETWEEN 1 and 3;
+    query = "select count(id) from eq WHERE gap between "+getsamll +" and "+ getlarge +" then;"
+    cursor.execute(query) 
+    rows = cursor.fetchall()
+    return render_template('q11.html',rows=rows)
 
 if __name__ == '__main__':
 	app.run(debug= True, port = 8000)
+
