1080b645ab7c9376dc029ce0bb0777067d72c866
Saurabh Marne <40160235+saurabhmarne9595@users.noreply.github.com>
2022-06-06 23:47:06 -0500

    Update app.py

--- ass3/app.py
+++ ass3/app.py
@@ -7,14 +7,14 @@ import matplotlib.pyplot as plt, pyodbc, base64, io
 server='tcp:adbass.database.windows.net'
 database = 'adbdb'
 username='sqladmin'
-password='S@urabh9595'
+password='my_password'
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = conn.cursor()
 app =  Flask(__name__)
 
-primary = 'kQv8CGbmCcVaFIfq3Uziom8RJbuNhevyDAzCaASW488='
-# secondary = 'RtdyFGXwimiZz6erOaDQOshtzRsls0kbVAzCaAp9Blo='
-# Primary_connection_string='adbass.redis.cache.windows.net:6380,password=kQv8CGbmCcVaFIfq3Uziom8RJbuNhevyDAzCaASW488=,ssl=True,abortConnect=False'
+primary = '---------------------------='
+# secondary = '----------------------------='
+# Primary_connection_string='adbass.redis.cache.windows.net:6380,password=------------------------------------=,ssl=True,abortConnect=False'
 
 # r = redis.Redis(host='adbass.redis.cache.windows.net', port=6380,  password=primary)
 
